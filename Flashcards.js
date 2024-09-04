4b080710ea6e65ec4aab343cbbae3ee5201b1697
Charles R. Dorner III <31493310+dornercr@users.noreply.github.com>
2024-09-03 22:10:34 -0400

    Update Flashcards.js

--- Flashcards.js
+++ Flashcards.js
@@ -1,1 +1,78 @@
-(function(_0xa09287,_0xa0acf0){const _0x1c5bdd=_0x2c49,_0x2ab730=_0xa09287();while(!![]){try{const _0x5ce409=-parseInt(_0x1c5bdd(0x1ea))/0x1*(parseInt(_0x1c5bdd(0x1fc))/0x2)+parseInt(_0x1c5bdd(0x1f7))/0x3+parseInt(_0x1c5bdd(0x205))/0x4*(-parseInt(_0x1c5bdd(0x209))/0x5)+parseInt(_0x1c5bdd(0x20d))/0x6+-parseInt(_0x1c5bdd(0x204))/0x7*(-parseInt(_0x1c5bdd(0x1df))/0x8)+parseInt(_0x1c5bdd(0x1f6))/0x9*(parseInt(_0x1c5bdd(0x1ed))/0xa)+-parseInt(_0x1c5bdd(0x203))/0xb;if(_0x5ce409===_0xa0acf0)break;else _0x2ab730['push'](_0x2ab730['shift']());}catch(_0x2f8c9d){_0x2ab730['push'](_0x2ab730['shift']());}}}(_0x14fa,0xb7dcf));function _0x2c49(_0x277ad7,_0x51774c){const _0x14fa49=_0x14fa();return _0x2c49=function(_0x2c490a,_0x490284){_0x2c490a=_0x2c490a-0x1de;let _0x4e6d40=_0x14fa49[_0x2c490a];return _0x4e6d40;},_0x2c49(_0x277ad7,_0x51774c);}function _0x14fa(){const _0x207d26=['Sorting\x20Algorithms','answer','3uLmqiy','mb-4','Show\x20Answer','348890eSsTkv','code','whatAreAlgorithms','text-center\x20mt-4\x20text-gray-600','flex\x20justify-between\x20p-4\x20bg-gray-50','\x20of\x20','text-lg\x20w-full\x20text-left','div','bg-blue-100\x20rounded-lg\x20p-4\x20min-h-[200px]\x20flex\x20items-start\x20justify-start\x20text-left','351rdeNUu','4425570vkEYDA','select','flex\x20items-center\x20text-blue-500\x20hover:text-blue-600','Show\x20Question','mt-4\x20w-full\x20bg-blue-500\x20text-white\x20py-2\x20rounded-lg\x20hover:bg-blue-600\x20transition\x20duration-300','574214EKogfe','pre','button','useState','createElement','Data\x20Structures\x20and\x20Algorithms','greedyAlgorithms','11789184tdcWKU','2240nAOovo','28vhRbOF','Computational\x20Complexity','option','length','407765EpYeYB','Card\x20','bg-white\x20rounded-lg\x20shadow-lg\x20overflow-hidden','text-xl\x20font-bold\x20mb-4\x20text-center','680028GzzYQS','target','7696VeqhbN','question','flex\x20flex-col\x20items-center\x20justify-center\x20min-h-screen\x20bg-gray-100\x20p-4','w-full\x20max-w-md','dataStructuresAndAlgorithms','w-full\x20p-2\x20rounded-lg\x20border\x20border-gray-300','What\x20Are\x20Algorithms?','Next','Search\x20Algorithms'];_0x14fa=function(){return _0x207d26;};return _0x14fa();}const Flashcards=()=>{const _0x3f5bae=_0x2c49,[_0x288cc1,_0x13711e]=React['useState'](_0x3f5bae(0x1e3)),[_0x3bf864,_0x56101f]=React[_0x3f5bae(0x1ff)](0x0),[_0x5312fc,_0x39cc13]=React['useState'](![]),_0x4d7b8f=flashcardsData[_0x288cc1],_0x37d528=()=>{const _0x40dd2b=_0x3f5bae;_0x56101f(_0x235cfd=>(_0x235cfd+0x1)%_0x4d7b8f[_0x40dd2b(0x208)]),_0x39cc13(![]);},_0x4dd0b8=()=>{const _0x141426=_0x3f5bae;_0x56101f(_0x441cea=>(_0x441cea-0x1+_0x4d7b8f[_0x141426(0x208)])%_0x4d7b8f[_0x141426(0x208)]),_0x39cc13(![]);},_0x1fb091=()=>{_0x39cc13(_0x479b6b=>!_0x479b6b);},_0x1443f0=_0x2164a9=>{_0x13711e(_0x2164a9),_0x56101f(0x0),_0x39cc13(![]);};return React[_0x3f5bae(0x200)]('div',{'className':_0x3f5bae(0x1e1)},React[_0x3f5bae(0x200)](_0x3f5bae(0x1f4),{'className':_0x3f5bae(0x1e2)},React[_0x3f5bae(0x200)]('div',{'className':_0x3f5bae(0x1eb)},React['createElement'](_0x3f5bae(0x1f8),{'value':_0x288cc1,'onChange':_0x164f93=>_0x1443f0(_0x164f93[_0x3f5bae(0x1de)]['value']),'className':_0x3f5bae(0x1e4)},React[_0x3f5bae(0x200)](_0x3f5bae(0x207),{'value':'dataStructuresAndAlgorithms'},_0x3f5bae(0x201)),React['createElement'](_0x3f5bae(0x207),{'value':'sortingAlgorithms'},_0x3f5bae(0x1e8)),React[_0x3f5bae(0x200)](_0x3f5bae(0x207),{'value':_0x3f5bae(0x1ef)},_0x3f5bae(0x1e5)),React[_0x3f5bae(0x200)](_0x3f5bae(0x207),{'value':'searchAlgorithms'},_0x3f5bae(0x1e7)),React[_0x3f5bae(0x200)](_0x3f5bae(0x207),{'value':'computationalComplexity'},_0x3f5bae(0x206)),React[_0x3f5bae(0x200)](_0x3f5bae(0x207),{'value':_0x3f5bae(0x202)},'greedyAlgorithms'))),React[_0x3f5bae(0x200)](_0x3f5bae(0x1f4),{'className':_0x3f5bae(0x20b)},React[_0x3f5bae(0x200)](_0x3f5bae(0x1f4),{'className':'p-6'},React[_0x3f5bae(0x200)]('h2',{'className':_0x3f5bae(0x20c)},(topicTitles[_0x288cc1]||'What\x20Are\x20Algorithms?')+'\x20Flashcards'),React[_0x3f5bae(0x200)](_0x3f5bae(0x1f4),{'className':_0x3f5bae(0x1f5)},React[_0x3f5bae(0x200)](_0x3f5bae(0x1f4),{'className':_0x3f5bae(0x1f3)},_0x5312fc?React[_0x3f5bae(0x200)](_0x3f5bae(0x1fd),{'className':'whitespace-pre-wrap'},React[_0x3f5bae(0x200)](_0x3f5bae(0x1ee),null,_0x4d7b8f[_0x3bf864][_0x3f5bae(0x1e9)])):React['createElement']('p',null,_0x4d7b8f[_0x3bf864][_0x3f5bae(0x1e0)]))),React['createElement'](_0x3f5bae(0x1fe),{'onClick':_0x1fb091,'className':_0x3f5bae(0x1fb)},_0x5312fc?_0x3f5bae(0x1fa):_0x3f5bae(0x1ec))),React[_0x3f5bae(0x200)](_0x3f5bae(0x1f4),{'className':_0x3f5bae(0x1f1)},React[_0x3f5bae(0x200)](_0x3f5bae(0x1fe),{'onClick':_0x4dd0b8,'className':_0x3f5bae(0x1f9)},'Previous'),React[_0x3f5bae(0x200)](_0x3f5bae(0x1fe),{'onClick':_0x37d528,'className':'flex\x20items-center\x20text-blue-500\x20hover:text-blue-600'},_0x3f5bae(0x1e6)))),React[_0x3f5bae(0x200)]('p',{'className':_0x3f5bae(0x1f0)},_0x3f5bae(0x20a)+(_0x3bf864+0x1)+_0x3f5bae(0x1f2)+_0x4d7b8f[_0x3f5bae(0x208)])));};
+const Flashcards = () => {
+  const [currentTopic, setCurrentTopic] = React.useState('dataStructuresAndAlgorithms');
+  const [currentCard, setCurrentCard] = React.useState(0);
+  const [showAnswer, setShowAnswer] = React.useState(false);
+  const flashcards = flashcardsData[currentTopic];
+
+  const nextCard = () => {
+    setCurrentCard((prev) => (prev + 1) % flashcards.length);
+    setShowAnswer(false);
+  };
+
+  const prevCard = () => {
+    setCurrentCard((prev) => (prev - 1 + flashcards.length) % flashcards.length);
+    setShowAnswer(false);
+  };
+
+  const toggleAnswer = () => {
+    setShowAnswer((prev) => !prev);
+  };
+
+  const changeTopic = (topic) => {
+    setCurrentTopic(topic);
+    setCurrentCard(0);
+    setShowAnswer(false);
+  };
+
+  return React.createElement('div', { className: "flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4" },
+    React.createElement('div', { className: "w-full max-w-md" },
+      React.createElement('div', { className: "mb-4" },
+        React.createElement('select', {
+          value: currentTopic,
+          onChange: (e) => changeTopic(e.target.value),
+          className: "w-full p-2 rounded-lg border border-gray-300"
+        },
+          React.createElement('option', { value: "dataStructuresAndAlgorithms" }, "Data Structures and Algorithms"),
+          React.createElement('option', { value: "sortingAlgorithms" }, "Sorting Algorithms"),
+          React.createElement('option', { value: "whatAreAlgorithms" }, "What Are Algorithms?"),
+          React.createElement('option', { value: "searchAlgorithms" }, "Search Algorithms"),
+          React.createElement('option', { value: "computationalComplexity" }, "Computational Complexity"),
+          React.createElement('option', { value: "greedyAlgorithms" }, "greedyAlgorithms")
+        )
+      ),
+      React.createElement('div', { className: "bg-white rounded-lg shadow-lg overflow-hidden" },
+        React.createElement('div', { className: "p-6" },
+          React.createElement('h2', { className: "text-xl font-bold mb-4 text-center" },
+            `${topicTitles[currentTopic] || 'What Are Algorithms?'} Flashcards`
+          ),
+          React.createElement('div', { className: "bg-blue-100 rounded-lg p-4 min-h-[200px] flex items-start justify-start text-left" },
+            React.createElement('div', { className: "text-lg w-full text-left" },
+              showAnswer ? 
+                React.createElement('pre', { className: "whitespace-pre-wrap" },
+                  React.createElement('code', null, flashcards[currentCard].answer)
+                ) :
+                React.createElement('p', null, flashcards[currentCard].question)
+            )
+          ),
+          React.createElement('button', {
+            onClick: toggleAnswer,
+            className: "mt-4 w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300"
+          }, showAnswer ? "Show Question" : "Show Answer")
+        ),
+        React.createElement('div', { className: "flex justify-between p-4 bg-gray-50" },
+          React.createElement('button', {
+            onClick: prevCard,
+            className: "flex items-center text-blue-500 hover:text-blue-600"
+          }, "Previous"),
+          React.createElement('button', {
+            onClick: nextCard,
+            className: "flex items-center text-blue-500 hover:text-blue-600"
+          }, "Next")
+        )
+      ),
+      React.createElement('p', { className: "text-center mt-4 text-gray-600" },
+        `Card ${currentCard + 1} of ${flashcards.length}`
+      )
+    )
+  );
+};
